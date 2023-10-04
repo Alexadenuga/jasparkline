fbcd9a735b087bff800d3ba928a778140e9ecdef
Alex Adenuga <alexadenuga@gmail.com>
2023-10-04 10:15:45 -0400

    fixes

--- jasp.R
+++ jasp.R
@@ -22,10 +22,10 @@ the_spark_plot <- function(dat, trans, col, label_x, label_y, the_title) {
   
   plot(1, type = "n", xlim = c(1, nrow(dat)), ylim = c(0, max(dat)), xlab = label_x, ylab = label_y, xaxt = "n")
   
-  # Plot the first column (X1) as a normal blue line
+  # Plot the first column (X1) as a normal line
   lines(1:nrow(dat), dat[, 1], col = col)
   
-  # Plot the remaining columns as transparent blue lines
+  # Plot the remaining columns as transparent lines
   for (i in 2:ncol(dat)) {
     lines(1:nrow(dat), dat[, i], col = rgb(1, 0, 0, alpha = trans))
   }
@@ -35,7 +35,7 @@ the_spark_plot <- function(dat, trans, col, label_x, label_y, the_title) {
   
   # Add axis labels
   axis(1, at = 1:nrow(dat), labels = 1:nrow(dat))
-  axis(2, at = seq(0, 1, by = 0.25))
+  axis(2, at = seq(0, 1, by = 0.5))
   
   # Add a legend
   legend("topright", legend = colnames(dat), col = c(col, rep(rgb(1, 0, 0, alpha = 0.5), ncol(dat) - 1)), lty = 1)
